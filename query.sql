44eaf35c52707a1d0cd252135dbc82c794345f67
lksngy <luk.nagy@gmail.com>
2021-02-06 11:53:22 +0100

    inner join, left join exercise

--- query.sql
+++ query.sql
@@ -1,5 +1,4 @@
--- copy of queries from DBeaver
-
+/*
 -- basic statement
 select a.emp_no,
 concat(a.first_name, a.last_name) as "name",
@@ -24,6 +23,22 @@ concat(a.first_name, a.last_name) as "name",
 b.salary, c.title
 from employees as a
 inner join salaries as b on a.emp_no = b.emp_no
-inner join titles as c on c.emp_no = a.emp_no
-and c.from_date = (b.from_date + interval '2 days')
+inner join titles as c 
+on 	c.emp_no = a.emp_no and (
+	c.from_date = (b.from_date + interval '2 days') or 
+	c.from_date = b.from_date
+)
 order by a.emp_no;
+*/
+
+-- see salary raises, that were both promotion and no promotion (left join)
+select a.emp_no,
+concat(a.first_name, a.last_name) as "name",
+b.salary, coalesce(c.title, 'no title change'), coalesce(c.from_date::text, '-')
+from employees as a
+inner join  salaries as b on a.emp_no = b.emp_no
+left join titles as c 
+	on a.emp_no = c.emp_no and (
+	c.from_date = (b.from_date + interval '2 days') or
+	c.from_date = b.from_date
+	)
